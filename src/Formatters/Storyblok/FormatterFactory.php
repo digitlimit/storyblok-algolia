70a20d2adcb7bf40d2063def287c386b3bbc0254
Emeka Mbah <frankemeks77@yahoo.com>
2022-01-26 10:57:43 +0100

    add formatter factory

---
+++ src/Formatters/Storyblok/FormatterFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Digitlimit\StoryblokAlgolia\Formatters\Storyblok;
+
+use Illuminate\Support\Str;
+use Exception;
+
+class FormatterFactory
+{
+    /**
+     * Create a new formatter instance.
+     */
+    public function make(string $type, array $story): Formatter
+    {
+        $class_name = Str::studly($type);
+        $class = "App\\Formatters\\Storyblok\\{$class_name}Formatter";
+
+        if (!$class_name || !class_exists($class)) {
+            throw new Exception("Formatter class {$class} does not exist");
+        }
+
+        return new $class($story);
+    }
+}
