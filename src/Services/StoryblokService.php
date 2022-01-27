757626a209660fe2e12d7ff48651909393d8b151
Emeka Mbah <frankemeks77@yahoo.com>
2022-01-27 09:06:07 +0100

    add informative logs in some lines

--- src/Services/StoryblokService.php
+++ src/Services/StoryblokService.php
@@ -7,6 +7,7 @@ use Digitlimit\StoryblokAlgolia\Helpers\StoryblokHelper;
 use Digitlimit\StoryblokAlgolia\Helpers\AlgoliaHelper;
 use Digitlimit\StoryblokAlgolia\Formatters\Storyblok\Formatter;
 use Digitlimit\StoryblokAlgolia\Formatters\Storyblok\FormatterFactory;
+use Digitlimit\StoryblokAlgolia\Helpers\Logger;
 use Exception;
 
 class StoryblokService
@@ -44,7 +45,7 @@ class StoryblokService
      */
     public function handleWebhook(Request $request)
     {
-        info('Storyblok webhook received');
+        Logger::info('Storyblok webhook received');
 
         try {
             $this->storyblokHelper->handleWebhook(
@@ -60,7 +61,7 @@ class StoryblokService
                 },
             );
         } catch (Exception $e) {
-            info('Storyblok webhook failed: ' . $e->getMessage());
+            Logger::info('Storyblok webhook failed: ' . $e->getMessage());
             return;
         }
 
@@ -107,30 +108,34 @@ class StoryblokService
      */
     protected function handlePublished(object $payload, StoryblokHelper $storyblok)
     {
+        Logger::info('Handle published Storyblok story -- start');
+
         $story = $storyblok
             ->getStory($payload->story_id);
 
-        // info(['$story' => $story]);
+        Logger::info(['story' => $story]);
 
         $page_type = $story['content']['component'];
 
-        // info(['$page_type' => $page_type]);
+        Logger::info(['page_type' => $page_type]);
 
         $formatterClass = $this->formatterFactory
             ->make($page_type, $story);
 
-        // info(['$formatterClass' => $formatterClass]);
+        Logger::info(['formatterClass' => $formatterClass]);
 
         $formatted_story = $this->formatter->setFormat(
             new $formatterClass($story)
         )->getFormat();
 
-        // info(['$formatted_story' => $formatted_story]);
+        Logger::info(['formatted_story' => $formatted_story]);
 
         $this->algoliaHelper
             ->add(strtolower($page_type), $formatted_story);
 
-        // info("Story published: {$story['content']['title']}");
+        Logger::info("Story published: {$story['content']['title']}");
+
+        Logger::info('Handle published Storyblok story -- done');
     }
 
     /**
@@ -140,19 +145,30 @@ class StoryblokService
      */
     protected function handleUnpublished(object $payload, StoryblokHelper $storyblok)
     {
+        Logger::info('Handle unpublished Storyblok story -- start');
+
         $story = $storyblok
             ->getStory($payload->story_id);
 
+        Logger::info(['story' => $story]);
+
         $object_id = $story['uuid'];
 
+        Logger::info(['object_id' => $object_id]);
+
         $page_type = $story['content']['component'];
+
+        Logger::info(['page_type' => $page_type]);
 
         $algolia_index = strtolower($page_type);
 
         // @todo check algolia index exists
         // if not create index
+        Logger::info(['algolia_index' => $algolia_index]);
 
         $this->algoliaHelper
             ->delete($algolia_index, $object_id);
+
+        Logger::info('Handle unpublished Storyblok story -- done');     
     }
 }
